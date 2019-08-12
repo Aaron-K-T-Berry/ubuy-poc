10a10726859bf6e17fe9dd4448328744296925ed
Aaron Berry <aaron.abm@gmail.com.au>
2019-08-12 15:34:24 +1000

    Resetting project

--- src/api/controllers/user/components/mongodb-client.ts
+++
@@ -1,5 +0,0 @@
-export default class MongodbClient {
-	constructor() {}
-
-	public isUnique(table: string, property: string, value: string) {}
-}
--- src/common/models/address-aus.model.ts
+++
@@ -1,7 +0,0 @@
-export default interface addressAUS {
-	street1: string;
-	street2: string;
-	suburb: string;
-	state: string;
-	postcode: string;
-}
