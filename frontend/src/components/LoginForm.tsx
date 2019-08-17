de63d7e8886b700ef99c37e37c001f8882c3078d
vong98 <scvong12@gmail.com>
2019-08-17 15:40:59 +1000

    button from bootstrap isn't working

---
+++ frontend/src/components/LoginForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+
+export default class LoginForm extends React.Component {
+    render() {
+        return (
+            <Button>LOGIN</Button>;
+        );
+    }
+}
