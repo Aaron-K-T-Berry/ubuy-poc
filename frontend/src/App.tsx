b6a2401136f8be7816d860d6c368a8208cb7b6eb
Vong Beng <scvong12@gmail.com>
2019-08-17 20:50:12 +1000

    created login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import "./styles/App.css";
 import LoginForm from "./components/LoginForm";
-import ItemViewer from "./components/ItemViewer";
 
 const App: React.FC = () => {
 	return (
-		<div className="page-wrapper">
-			<h1 className="h1">U-Buy</h1>
-
-			<div className="container">
-				<div>
-					<h2>Add Item</h2>
+		<div className="page-wrapper" id="parent">
+			<div className="h1">uBuy</div>
+			
+			<div className="container" id="login">
+				<div className="login">
 					<LoginForm />
-				</div>
-
-				<div>
-					<h2>All Items</h2>
-					<ItemViewer />
 				</div>
 			</div>
 		</div>
--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Label from 'react-bootstrap/FormLabel'
 
 export default class LoginForm extends React.Component {
     render() {
         return (
-            <Button>LOGIN</Button>
+            <Form>
+                <FormGroup controlId="formEmail">
+                    <Label> Email Address:</Label>
+                    <FormControl type="text" placeholder="name@example.com"/>
+                </FormGroup>
+
+                <FormGroup controlId="formPassword">
+                    <Label> Password: </Label>
+                    <FormControl type="password" placeholder="Password"/>
+                </FormGroup>
+
+                <Label id="labelFeedback"></Label>
+                <Button id="buttonLogin" block>Log In</Button>
+            </Form>
         );
     }
 }
