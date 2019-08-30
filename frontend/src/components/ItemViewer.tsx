ca27619de2bc6987f4aaa5baff27bcae74bd9142
Vong Beng <scvong12@gmail.com>
2019-08-31 00:21:37 +1000

    FINALLY got the dynamic card display to work by iterating over array

--- frontend/src/components/ItemViewer.tsx
+++ frontend/src/components/ItemViewer.tsx
@@ -1,106 +1,76 @@
 import React from "react";
 import "../styles/App.css";
-import {Card, Button, Container, Row, Col} from "react-bootstrap";
-let items:Card[] = [];
+import ItemCard from "./ItemCard";
+import {Container, Row} from "react-bootstrap";
 
-export default class ItemViewer extends React.Component {
+export interface ItemState {
+    items: {
+            name: string,
+            price: string,
+            desc: string,
+            img: string
+    }[]    
+}
+
+export default class ItemViewer extends React.Component<{}, ItemState> {
     constructor(props: any){
         super(props);
-        
+        this.state = {
+            items: [
+                {
+                    name: "nice mouse bro",
+                    price: "7.99",
+                    desc: "wow",
+                    img: "mouse.jpg"
+                },
+                {
+                    name: "wow rays",
+                    price: "11.99",
+                    desc: "let there be light",
+                    img: "rtx.jpg"
+                },
+                {
+                    name: "greatest mouse of all time",
+                    price: "1199.99",
+                    desc: "GOAT",
+                    img: "ball_mouse.jpg"
+                },
+                {
+                    name: "cheese grater",
+                    price: "5999.99",
+                    desc: "that is base price btw.",
+                    img: "cheesegrater.jpg"
+                },
+                {
+                    name: "xDr PrO dIsPlAy",
+                    price: "4999.99",
+                    desc: "The first 32-inch Retina 6K display ever. Over a billion colours presented with exceptional accuracy." +
+                            "Introducing Apple Pro Display XDR, the world’s best pro display. *Monitor stand not included.",
+                    img: "xD.jpg"
+                },
+                {
+                    name: "monitor stand",
+                    price: "999.99",
+                    desc: "monitor stand included",
+                    img: "stand_XD.jpg"
+                }
+            ]
+        }
     }
-
-    /* 
-    addItem(name : string, price : string, desc : string, img : string) {
-        items.push(
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={require(img)} />
-                <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Subtitle>${price}</Card.Subtitle>
-                    <Card.Text>
-                        {desc}}
-                    </Card.Text>
-                    <Button variant="primary">Add To Cart</Button>
-                </Card.Body>
-            </Card>
-        );
-    }
-
-    returnItems(){
-        return (
-            <div>
-                {items}
-            </div>
-        );
-    } */
-
-
     render() {
-      return (
-        <div >
-            <Container fluid>
-                <Row noGutters={true} className="justify-content-md-center">
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={require('../mouse_placeholder.jpg')} />
-                        <Card.Body>
-                            <Card.Title>gaeming mouse</Card.Title>
-                            <Card.Subtitle>$7.99</Card.Subtitle>
-                            <Card.Text>
-                                uNtEtHeReD. uNlEaShED. uNsToPpABlE.
-                            </Card.Text>
-                            <Button variant="primary">Add To Cart</Button>
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={require('../mouse_placeholder.jpg')} />
-                        <Card.Body>
-                            <Card.Title>gaeming mouse</Card.Title>
-                            <Card.Subtitle>$7.99</Card.Subtitle>
-                            <Card.Text>
-                                uNtEtHeReD. uNlEaShED. uNsToPpABlE.
-                            </Card.Text>
-                            <Button variant="primary">Add To Cart</Button>
-                        </Card.Body>
-                    </Card>
-
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={require('../mouse_placeholder.jpg')} />
-                        <Card.Body>
-                            <Card.Title>gaeming mouse</Card.Title>
-                            <Card.Subtitle>$7.99</Card.Subtitle>
-                            <Card.Text>
-                                uNtEtHeReD. uNlEaShED. uNsToPpABlE.
-                            </Card.Text>
-                            <Button variant="primary">Add To Cart</Button>
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={require('../mouse_placeholder.jpg')} />
-                        <Card.Body>
-                            <Card.Title>gaeming mouse</Card.Title>
-                            <Card.Subtitle>$7.99</Card.Subtitle>
-                            <Card.Text>
-                                uNtEtHeReD. uNlEaShED. uNsToPpABlE.
-                            </Card.Text>
-                            <Button variant="primary">Add To Cart</Button>
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={require('../mouse_placeholder.jpg')} />
-                        <Card.Body>
-                            <Card.Title>gaeming mouse</Card.Title>
-                            <Card.Subtitle>$7.99</Card.Subtitle>
-                            <Card.Text>
-                                uNtEtHeReD. uNlEaShED. uNsToPpABlE.
-                            </Card.Text>
-                            <Button variant="primary">Add To Cart</Button>
-                        </Card.Body>
-                    </Card>
-                    {/* Replace this with actual card loader later*/}
-                </Row>
-            </Container>
-            
-        </div>
+        let itemCards = this.state.items.map(item =>{
+            return (
+                <ItemCard item={item} />
+            )
+        })
+        return (
+            <div >
+                <Container fluid>
+                    <Row noGutters={true} className="justify-content-md-center">
+                        {itemCards}
+                    </Row>
+                </Container>
+            </div>
       );
     }
 }
