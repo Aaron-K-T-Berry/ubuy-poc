a4ae3eeab9662ca110943fd00da0afbf9b36fea7
Aaron Berry <aaron.abm@gmail.com.au>
2019-09-02 17:47:46 +1000

    :sparkles: (backend) added 404 for any unkown paths

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -10,4 +10,25 @@ export const Routes = (init: ApiInitializer) => {
 	AuthRoute(init.getApp(), new AuthController());
 	UserRoute(init.getApp(), new UserController());
 	BranchRoute(init.getApp(), new BranchController());
+
+	// 404
+	init.getApp().use(function(req, res, next) {
+		res.status(404);
+
+		// respond with html page
+		// Todo: Support html rendering of server pages
+		// if (req.accepts("html")) {
+		// 	res.render("404", { url: req.url });
+		// 	return;
+		// }
+
+		// respond with json
+		if (req.accepts("json")) {
+			res.send({ error: "Not found" });
+			return;
+		}
+
+		// default to plain-text. send()
+		res.type("txt").send("Not found");
+	});
 };
