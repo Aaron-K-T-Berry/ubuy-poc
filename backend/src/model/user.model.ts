47e8155965e4f0b8d05d772194e92693847b599a
Aaron Berry <aaron.abm@gmail.com.au>
2019-09-03 06:52:32 +1000

    :sparkles: (backend) updated user model for extended meta data

--- backend/src/model/user.model.ts
+++ backend/src/model/user.model.ts
@@ -1,37 +1,37 @@
-import mongoose, { Document } from "mongoose";
+import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 const saltRounds = 10;
 
-export interface User {
+export interface UserModel {
 	firstName: string;
 	lastName: string;
 	email: string;
-	// customer | admin | employee
-	// TODO validate against this
-	address: string;
-	userType: string;
+	// Todo i feel like this needs to be handled better
+	password?: string;
+	address: string | undefined;
+	userMeta: InternalUserType | undefined;
+	isCorrectPassword?: Function;
 }
 
-export interface UserModel extends Document {
-	firstName: string;
-	lastName: string;
-	email: string;
-	password: string;
-	isCorrectPassword: Function;
+export interface InternalUserType {
+	type: string; // admin or employee
+	branchID: string | undefined; // id of branch or undefined for admin
 }
 
 const UserSchema = new mongoose.Schema({
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true }
+	password: { type: String, required: true },
+	address: { type: String },
+	userMeta: { type: Object }
 });
 
 UserSchema.pre("save", function(next) {
 	// Check if document is new or a new password has been set
 	if (this.isNew || this.isModified("password")) {
-		// Saving reference to this because of changing scopes
 		// @ts-ignore
+		// Saving reference to this because of changing scopes
 		const document: UserModel = this;
 		bcrypt.hash(document.password, saltRounds, function(err, hashedPassword) {
 			if (err) {
