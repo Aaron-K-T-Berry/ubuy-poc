b6e39b9a329fb13e2214e4cf63c859e4460e3561
vong98 <scvong12@gmail.com>
2019-10-02 17:38:33 +1000

    Added View All Items Page and changed item placeholder names

---
+++ frontend/src/components/ItemViewer/ItemViewerTable.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "../../styles/App.css";
+import dummy_data, { ItemRow as Item } from "./data/itemsStub";
+import Table from 'react-bootstrap/Table';
+import ItemRow from "./ItemRow";
+
+export interface ItemState {
+	items: Item[];
+}
+
+export default class ItemViewerTable extends React.Component<{}, ItemState> {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			items: dummy_data
+		};
+    }
+    
+	render() {
+		return (
+            <Table striped bordered>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Description</th>
+                        <th>Branch</th>
+                        <th>View/Edit/Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.items.map(item => {
+                        return <ItemRow item={item} />
+                    })}
+                </tbody>
+            </Table>
+		);
+	}
+}
