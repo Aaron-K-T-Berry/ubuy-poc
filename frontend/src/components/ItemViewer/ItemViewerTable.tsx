7f98bad3156c14bc36fd7675ee9c0b4d4fec7189
Aaron-K-T-Berry <aaron.abm@gmail.com>
2019-10-04 00:13:05 +1000

    cleanup

--- frontend/src/components/ItemViewer/ItemViewerTable.tsx
+++ frontend/src/components/ItemViewer/ItemViewerTable.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../../styles/App.css";
 import "./styles/ItemViewerTable.css";
 import dummy_data, { ItemRow as Item } from "./data/itemsStub";
-import Table from 'react-bootstrap/Table';
+import Table from "react-bootstrap/Table";
 import ItemRow from "./ItemRow";
 
 export interface ItemState {
@@ -15,27 +15,27 @@ export default class ItemViewerTable extends React.Component<{}, ItemState> {
 		this.state = {
 			items: dummy_data
 		};
-    }
-    
+	}
+
 	render() {
 		return (
-            <Table striped bordered className="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Description</th>
-                        <th>Branch</th>
-                        <th>View/Edit/Remove</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.items.map(item => {
-                        return <ItemRow item={item} />
-                    })}
-                </tbody>
-            </Table>
+			<Table striped bordered className="table">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Price</th>
+						<th>Description</th>
+						<th>Branch</th>
+						<th>View/Edit/Remove</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.state.items.map(item => {
+						return <ItemRow item={item} />;
+					})}
+				</tbody>
+			</Table>
 		);
 	}
 }
--- frontend/src/pages/ViewAllItems.tsx
+++ frontend/src/pages/ViewAllItems.tsx
@@ -1,28 +1,27 @@
 import React from "react";
 import "../styles/App.css";
-import ItemTable from "../components/ItemViewer/ItemViewerTable";
+import ItemTable from "../Components/ItemViewer/ItemViewerTable";
 
-export interface ViewItemState {
+export interface ViewItemState {}
 
-}
+export interface ViewItemProps {}
 
-export interface ViewItemProps {
+export default class ViewAllItems extends React.Component<
+	ViewItemProps,
+	ViewItemState
+> {
+	constructor(props: any) {
+		super(props);
+	}
 
-}
-
-export default class ViewAllItems extends React.Component<ViewItemProps, ViewItemState> {
-    constructor(props: any){
-        super(props);
-    }
-
-    render() {
-        return(
-            <div className="content-body">
-                <div className="body-heading">View All Items</div>
-                <div>
-                    <ItemTable />
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="content-body">
+				<div className="body-heading">View All Items</div>
+				<div>
+					<ItemTable />
+				</div>
+			</div>
+		);
+	}
 }
