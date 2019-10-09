a827a7cc01644baccd313efb1513c637cad1d1fb
Rico-sudo <albericosss@hotmail.it>
2019-10-09 16:15:16 +1100

    np

---
+++ frontend/src/components/AccountsViewer/data/UsersStub.tsx
@@ -0,0 +1,62 @@
+export default [
+	{
+		firstName: "Alby",
+		lastName: "olly",
+		email: "lol@lol.com",
+		password: "123",
+	
+		address: "main st 123"
+	},
+	{
+		firstName: "Alby",
+		lastName: "olly",
+		email: "lol@lol.com",
+		password: "123",
+	
+		address: "main st 123"
+	},{
+		firstName: "Alby",
+		lastName: "olly",
+		email: "lol@lol.com",
+		password: "123",
+	
+		address: "main st 123"
+	},{
+		firstName: "Alby",
+		lastName: "olly",
+		email: "lol@lol.com",
+		password: "123",
+	
+		address: "main st 123"
+	},{
+		firstName: "Alby",
+		lastName: "olly",
+		email: "lol@lol.com",
+		password: "123",
+	
+		address: "main st 123"
+	},{
+		firstName: "Alby",
+		lastName: "olly",
+		email: "lol@lol.com",
+		password: "123",
+	
+		address: "main st 123"
+	},
+];
+
+export interface User {
+	firstName: string;
+	lastName: string;
+	email: string;
+	password?: string;
+	address: string | undefined;
+}
+
+export interface UserRow{
+	firstName: string;
+	lastName: string;
+	email: string;
+	password?: string;
+	address: string | undefined;
+}
---
+++ frontend/src/components/AccountsViewerSeller/data/SellersStub.tsx
@@ -0,0 +1,40 @@
+export default [
+	{
+id: "1",
+		name: "Alby",
+		address: "main st 123"
+	},
+	{
+		id: "2",
+				name: "Alby",
+				address: "main st 123"
+			},{
+				id: "3",
+						name: "Alby",
+						address: "main st 123"
+					},{
+						id: "4",
+								name: "Alby",
+								address: "main st 123"
+							},{
+								id: "5",
+										name: "Alby",
+										address: "main st 123"
+									},{
+										id: "6",
+												name: "Alby",
+												address: "main st 123"
+											},
+];
+
+export interface User {
+	id: string;
+	name: string;
+	address: string;
+}
+
+export interface SellerRow{
+	id: string;
+	name: string;
+	address: string;
+}
